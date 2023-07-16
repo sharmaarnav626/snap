5abb6cb202850c6182a664d1d43ad072f538653b
Arnav Sharma <95161759+sharmaarnav626@users.noreply.github.com>
2023-07-16 22:09:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const navLinks = document.querySelectorAll(".nav-links li");
+const ham = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const overflow = document.querySelector(".overflow");
+const mobileLinks = document.querySelectorAll(".mobile-links li");
+
+ham.addEventListener("click", () => {
+  const icon = document.querySelector(".mobile-menu-btn img");
+  ham.classList.toggle("active-btn");
+  mobileMenu.classList.toggle("active-menu");
+  if (ham.classList.contains("active-btn")) {
+    icon.src = "./images/icon-close-menu.svg";
+  } else {
+    icon.src = "./images/icon-menu.svg";
+  }
+});
+
+navLinks.forEach((link) => {
+  const subMenu = link.querySelector(".sub-menu");
+  link.addEventListener("click", () => {
+    link.classList.toggle("active");
+    subMenu.classList.toggle("active-menu");
+  });
+});
+mobileLinks.forEach((link) => {
+  const subMenu = link.querySelector(".mobile-sub");
+  link.addEventListener("click", () => {
+    link.classList.toggle("active");
+    subMenu.classList.toggle("active-menu");
+  });
+});
